8d48d85da9dde9ed6fada159b7fe87f683167510
MRHemel <hemelrahman80@gmail.com>
2023-04-30 22:07:16 +0600

    product displaying done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
-// import React from 'react';
-
-import { useEffect } from "react";
-import { useState } from "react";
+import './Header.css'
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
+
     return (
         <div>
-            <h1>hello:{products.length}</h1>
+            <nav className="nav">
+                <img src={logo} alt="" />
+                <div className="nav-items">
+                    <a href="/shop">Shop</a>
+                    <a href="/about_us">About us</a>
+                    <a href="/inventory">Inventory</a>
+                    <a href="/contacts">Contacts</a>
+                </div>
+            </nav>
         </div>
     );
 };
