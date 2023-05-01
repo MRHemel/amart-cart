e8ba94dce4d9b78733e48bc4c45cb432a8859f79
MRHemel <hemelrahman80@gmail.com>
2023-05-01 06:35:37 +0600

    calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const handleAddToCart = (product) => {
+        // console.log(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                 }
 
             </div>
             <div className="cart-container">
-                <h2>Order Summery</h2>
+                <Cart cart={cart}></Cart>
             </div>
 
         </div>
